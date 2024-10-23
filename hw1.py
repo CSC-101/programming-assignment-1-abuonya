9a23262b2472cd82528dd0b38276f80ac0d0517e
annalisebuonya <abuonya@calpoly.edu>
2024-10-22 22:12:12 -0700

    vowel count update

--- hw1.py
+++ hw1.py
@@ -3,6 +3,17 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
+# Design Recipe:
+# Purpose: This function computes and returns the count of the number of vowels (upper and lower) that appear in the input string.
+# Input: string , Output: integer
+# Example Input:  'meow' , Output Given the Input : 2
+# Name of function: vowel_count
+# read through a list, sort out vowels and put them into list, return the length of that list to get how many vowels
+
+def vowel_count(word: str) -> int:
+    vowel_list = list(word)
+    print(vowel_list)
+
 
 
 # Part 2
